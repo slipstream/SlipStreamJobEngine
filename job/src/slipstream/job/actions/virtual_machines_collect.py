ad62f5fd35fc2b57bb6cf574a46d8bace28e2c80
Eric Le Goff <elegoff@sixsq.com>
2018-09-11 15:54:08 +0200

    Add billable flag to collected vms

--- job/src/slipstream/job/actions/virtual_machines_collect.py
+++ job/src/slipstream/job/actions/virtual_machines_collect.py
@@ -118,6 +118,9 @@ class VirtualMachinesCollectJob(object):
                 return True
         return False
 
+    def is_billable(self, vm_state):
+        return vm_state.lower() in ['running', 'pending']     
+
     @staticmethod
     def dict2tuple(d, *keys):
         return tuple([d[k] for k in keys])
@@ -270,6 +273,7 @@ class VirtualMachinesCollectJob(object):
                    'connector': {'href': self.cloud_name},
                    'instanceID': vm_id,
                    'state': vm_state.lower(),
+                   'billable': is_billable(vm_state),
                    'credentials': [{'href': self.cloud_credential['id']}],
                    'acl': {'owner': {'type': 'ROLE', 'principal': 'ADMIN'}},
                    'serviceOffer': {'href': service_offer.get('id'),
